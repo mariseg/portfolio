fa45a7f997fbdffdb74287d1ca2284b6fa96314e
maricelsegal@gmail.com <maricelsegal@gmail.com>
2021-08-17 20:24:56 -0300

    add

--- footer.php
+++ footer.php
@@ -1,25 +1,4 @@
-<!-- contacto / index
-<footer class="footer container">               
-        <div class="ms-sm-0 ms-5 mt-5 pb-4">
-            <div class="row">
-                <div class="col-12 col-sm-3">
-                    <a href="https://github.com" target="_blank" title="GitHub"><i class="fab fa-github"></i></a>
-                    <a href="https://linkedin.com/in/maricel-segal-071113218/" target="_blank" title="Linkedin"><i
-                            class="fab fa-linkedin-in"></i></a>
-                </div>
-                <div class="col-12 col-sm-3">
-                    Sponsor <a href="https://depcsuite.com">DePc Suite</a>
-                </div>
-                <div class="col-12 col-sm-3">
-                    <a href="mailto:maricelsegal@gmail.com">maricelsegal@gmail.com</a>
-                </div>
-            </div>
-    </footer>
-    <div class="whatsapp">
-        <a href="https://api.whatsapp.com/send?phone=541156654836" target="_blank"
-            title="Enviar mensaje directo Whatsapp"><i class="fab fa-whatsapp"></i></a>
-    </div>
-    -->
+
 
     <footer class="footer <?php echo ($page == "proyectos" || $page == "sobre-mi" || $page == "projects" || $page == "about-me")? "fixed-bottom ms-4 ms-sm-0 mb-4" : "container";?>">
         <div class= "<?php echo ($page == "proyectos" || $page == "sobre-mi" || $page == "projects" || $page == "about-me")? "container" : "ms-sm-0 ms-5 mt-5 pb-4";?>">
